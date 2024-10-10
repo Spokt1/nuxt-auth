870be66c50874cecef4b0e3e5d655ea519de339d
Spokt1 <yurakak@yandex.ru>
2024-10-11 00:55:28 +0300

    Update useAuth.ts

--- src/runtime/composables/local/useAuth.ts
+++ src/runtime/composables/local/useAuth.ts
@@ -135,15 +135,17 @@ async function getSession(getSessionOptions?: GetSessionOptions): Promise<Sessio
 
   const config = useTypedBackendConfig(useRuntimeConfig(), 'local')
   const { path, method } = config.endpoints.getSession
-  const { data, loading, lastRefreshedAt, rawToken, token: tokenState, _internal } = useAuthState()
+  const { data, loading, lastRefreshedAt, rawToken, refreshToken, token: tokenState, _internal } = useAuthState()
 
   let token = tokenState.value
   // For cached responses, return the token directly from the cookie
   token ??= formatToken(_internal.rawTokenCookie.value, config)
 
-  if (!token && !getSessionOptions?.force) {
+  if (!token && !getSessionOptions?.force && !refreshToken) {
     loading.value = false
     return
+  } else if(!token && refreshToken){
+      return await refresh(getSessionOptions)
   }
 
   const headers = new Headers(token ? { [config.token.headerName]: token } as HeadersInit : undefined)
